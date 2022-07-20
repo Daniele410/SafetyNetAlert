531fa32bfd3850752b27505f156dbedc0219ac39
Danozzo <miraglia.daniele410@gmail.com>
2022-07-20 12:33:55 +0200

    fix project

--- src/main/java/com/safetyNetAlert/safetyNetAlert/SafetyNetAlertApplication.java
+++ src/main/java/com/safetyNetAlert/safetyNetAlert/SafetyNetAlertApplication.java
@@ -2,7 +2,9 @@ package com.safetyNetAlert.safetyNetAlert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Start program Whit SpringBootApplication
+ */
 @SpringBootApplication
 public class SafetyNetAlertApplication {
 
